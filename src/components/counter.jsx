f13c3283947ee3f7b493c7afc54bbfef3477eb64
Jeffrey Feng Lu <lufg@mail.uc.edu>
2020-09-24 16:05:39 -0400

    component tree

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,39 +2,36 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
-    tags: ["tag1", "tag2", "tag3"],
+    value: this.props.value,
+    // tags: ["tag1", "tag2", "tag3"],
   };
 
   formatCount() {
-    return this.state.count === 0 ? "Zero" : this.state.count;
+    return this.state.value === 0 ? "Zero" : this.state.value;
   }
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>No tags</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
 
   render() {
     return (
       <div>
+        <h4>{this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        {this.renderTags()}
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 }
