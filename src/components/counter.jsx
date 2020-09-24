95abfb0720857586ac582cb5220b5b39e68a22bf
Jeffrey Feng Lu <lufg@mail.uc.edu>
2020-09-24 14:32:51 -0400

    counter app first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  formatCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>No tags</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        {this.renderTags()}
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+}
+
+export default Counter;
